25394b2f5a35aba2f6ba27c8780d89f3b8f137d4
JES\jes-t <jestho01easv365.dk>
2026-09-14 13:25:10 +0200

    works

---
+++ SuperChocolateMilk.Core/MilkRecipeCalculator.cs
@@ -0,0 +1,21 @@
+﻿namespace SuperChocolateMilk.Core;
+
+public class MilkRecipeCalculator
+{
+    public static decimal CalculateChocolateSyrupRequired(int milkVolumeM1, string richnessLevel)
+    {
+        if (milkVolumeM1 <= 0) return 0m;
+
+        decimal baseSyrupM1 = milkVolumeM1 * 0.1m;
+
+        return richnessLevel.ToUpper() switch
+        {
+            "Light" => baseSyrupM1 * 0.75m,
+            "regular" => baseSyrupM1,
+            "Extra" => baseSyrupM1 * 1.5m,
+            "Ultra_Choco" => baseSyrupM1 * 2.0m,
+            _ => baseSyrupM1
+        };
+
+    }
+}
---
+++ SuperChocolateMilk.UnitTests/MilkRecipeCalculatorTests.cs
@@ -0,0 +1,32 @@
+﻿namespace SuperChocolateMilk.UnitTests;
+
+using Xunit;
+using SuperChocolateMilk.Core;
+
+public class MilkRecipeCalculatorTests
+{
+    [Fact]
+    public void CalculateChocolateSyrup_RegularRichness_ReturnsTenPercentRatio()
+    {
+
+        int milkVolume = 100;
+        string richness = "Regular";
+
+        decimal result = MilkRecipeCalculator.CalculateChocolateSyrupRequired(milkVolume, richness);
+        Assert.Equal(100m, result);
+    }
+
+    [Theory]
+    [InlineData(1000, "Light", 75)]
+    [InlineData(1000, "Extra", 150)]
+    [InlineData(1000, "ultra_choco", 200)]
+    [InlineData(0, "Regular", 0)]
+
+    public void CalculateChocolateSyrup_VariousScenarious_ReturnsExpectedAmount(
+        int milkVolume, string richness, int expectedSyrup)
+    {
+        decimal actualresult = MilkRecipeCalculator.CalculateChocolateSyrupRequired(milkVolume, richness);
+        
+        Assert.Equal(expectedSyrup, actualresult);
+    }
+}   
